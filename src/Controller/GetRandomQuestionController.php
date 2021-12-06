0c5500bf2a5a055199d1a6af83d25f1318955c59
Michał Stancelewski <michal@stancelewski.pl>
2021-12-06 22:20:20 +0100

    added response code

--- src/Controller/GetRandomQuestionController.php
+++ src/Controller/GetRandomQuestionController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Repository\QuestionRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 
 #[AsController]
 class GetRandomQuestionController extends AbstractController
@@ -18,17 +20,17 @@ class GetRandomQuestionController extends AbstractController
         $this->questionRepository = $questionRepository;
     }
 
-    public function __invoke(Request $request): array
+    public function __invoke(Request $request): JsonResponse
     {
         $randomQuestions = array();
         $numberOfQuestions = $request->get('number');
         $allQuestions = $this->questionRepository->findAll();
 
         if(!is_numeric($numberOfQuestions) || $numberOfQuestions <= 0){
-            return ["ERROR. REQUESTED A PARAMETER WITH VALUE BETWEEN 1 AND " . count($allQuestions) . "."];
+            return new JsonResponse("ERROR. REQUESTED A PARAMETER WITH VALUE BETWEEN 1 AND " . count($allQuestions) . ".", 400);
         }
         if(count($allQuestions) < $numberOfQuestions){
-            return ["ERROR. MAXIMUM NUMBER OF UNIQUE QUESTIONS IS: " . count($allQuestions) . "."];
+            return new JsonResponse("ERROR. MAXIMUM NUMBER OF UNIQUE QUESTIONS IS: " . count($allQuestions) . ".", 400);
         }
 
         for ($i = 0; $i < $numberOfQuestions; $i++){
@@ -39,7 +41,10 @@ class GetRandomQuestionController extends AbstractController
                 $i--;
             }
         }
-        return $randomQuestions;
+
+        $randomQuestions = $this->get('serializer')->serialize($randomQuestions, 'json');
+
+        return new JsonResponse($randomQuestions, 200, [],true);
     }
 
 }
